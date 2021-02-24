145a56b80b8e1e9db2e6cdd47eaf718c2028c7af
tanxian <xian.tan@ucarinc.com>
2021-02-24 14:58:05 +0800

    test1

--- src/test/java/com_api_auto/StringBufferTester.java
+++ src/test/java/com_api_auto/StringBufferTester.java
@@ -2,7 +2,7 @@ package com_api_auto;
 
 public class StringBufferTester {
     public static void main(String[] args) {
-        String url = "http://www.xxxx.xxx";
+        String url = "http://www.baidu.com";
         StringBuffer sb = new StringBuffer();
     }
 }
