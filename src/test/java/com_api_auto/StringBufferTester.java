ebc96a5276b4580dfbb2e63668e973a9ee922db5
tanxian <xian.tan@ucarinc.com>
2021-02-24 15:26:24 +0800

    第二次上传到master分支

--- src/test/java/com_api_auto/StringBufferTester.java
+++ src/test/java/com_api_auto/StringBufferTester.java
@@ -2,7 +2,6 @@ package com_api_auto;
 
 public class StringBufferTester {
     public static void main(String[] args) {
-        //上传到master分支
         String url = "http://www.baidu111.com";
         StringBuffer sb = new StringBuffer();
     }
