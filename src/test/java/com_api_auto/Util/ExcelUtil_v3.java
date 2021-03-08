1cce251d0d236db5e082b76266c395e00af91e2d
tanxian <304492560@qq.com>
2021-03-09 01:48:46 +0800

    更新poi依赖版本，并完成Excel数据读写

--- src/test/java/com_api_auto/Util/ExcelUtil_v3.java
+++ src/test/java/com_api_auto/Util/ExcelUtil_v3.java
@@ -48,10 +48,19 @@ public class ExcelUtil_v3 {
         return datas;
     }
 
+    /**
+     *
+     * @param excelPath 读取文件的目录
+     * @param SheetNum  读取文件的sheet
+     * @param CaseId    需要写入的CaseId编号
+     * @param CellNum   需要写入的列
+     * @param result    需要写入的结果
+     */
     public static void WriteData(String excelPath, int SheetNum, String CaseId, int CellNum, String result){
 
         try {
-            InputStream inp = ExcelUtil_v3.class.getResourceAsStream(excelPath);
+//            InputStream inp = ExcelUtil_v3.class.getResourceAsStream(excelPath);
+            InputStream inp = new FileInputStream(new File(excelPath));
             Workbook workbook = WorkbookFactory.create(inp);
             Sheet sheet = workbook.getSheetAt(SheetNum - 1);
             int lastRowNum = sheet.getLastRowNum();
@@ -66,7 +75,7 @@ public class ExcelUtil_v3 {
                     cellToBeWrite.setCellValue(result);
                 }
             }
-            OutputStream OutputStream = new FileOutputStream(new File("target/ApiInfo.xlsx"));
+            OutputStream OutputStream = new FileOutputStream(new File(excelPath));
             workbook.write(OutputStream);
         } catch (IOException e) {
             e.printStackTrace();
